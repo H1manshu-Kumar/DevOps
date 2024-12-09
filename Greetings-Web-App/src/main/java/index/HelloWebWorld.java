6e1da4929067ae05d5fe887fa9acbda6308d8d34
H1manshu-Kumar <himanshu.kumar244@gmail.com>
2024-12-09 19:50:36 +0530

    code updated

--- Greetings-Web-App/src/main/java/index/HelloWebWorld.java
+++ Greetings-Web-App/src/main/java/index/HelloWebWorld.java
@@ -9,32 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloWebWorld
- */
 @WebServlet("/HelloWebWorld")
 public class HelloWebWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public HelloWebWorld() {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter pw = response.getWriter();
 		response.setContentType("text/html");
 		pw.println("<h1>Hello Web World Greeting!!</h1>");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
