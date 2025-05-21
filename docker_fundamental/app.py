dd8d998b28d499854740483a6dd920b8fef672cb
Himanshu Kumar <36623672+H1manshu-Kumar@users.noreply.github.com>
2025-05-21 18:27:26 +0530

    Modified the code for app.py :hammer:

--- docker_fundamental/app.py
+++ docker_fundamental/app.py
@@ -1,10 +1,25 @@
+# Importing the Flask class from the flask module
 from flask import Flask
+
+# Creating an instance of the Flask application
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Dosto, welcome to DevOps Zero To Hero (Junoon  Batch 9)'
+    """
+    Root endpoint of the application.
+    
+    Returns:
+        str: A welcome message for visitors accessing the root URL.
+    """
+    return 'Hello Dosto, welcome to DevOps Zero To Hero (Junoon Batch 9)'
 
 @app.route('/health')
 def health():
+    """
+    Health check endpoint.
+    
+    Returns:
+        str: A message indicating that the server is running properly.
+    """
     return 'Server is up and running'
