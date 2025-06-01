9ed7e72da2ad774dcb201be7d9055114476d8528
Himanshu Kumar <36623672+H1manshu-Kumar@users.noreply.github.com>
2025-06-01 22:08:43 +0530

    Update HelloWebWorld.java

--- Java_EndToEnd_Project/first-web-app/src/main/java/indexPage/HelloWebWorld.java
+++ Java_EndToEnd_Project/first-web-app/src/main/java/indexPage/HelloWebWorld.java
@@ -9,23 +9,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// Define a servlet mapped to the URL pattern "/HelloWebWorld"
 @WebServlet("/HelloWebWorld")
 public class HelloWebWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	// Constructor for the servlet
 	public HelloWebWorld() {
 		super();
 	}
 
+	// Handles HTTP GET requests
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		PrintWriter pw = response.getWriter();
+		// Set the content type of the response to HTML
 		response.setContentType("text/html");
-  // Print message on Web Page
+
+		// Get the PrintWriter to write the HTML response
+		PrintWriter pw = response.getWriter();
+
+		// Send a simple HTML response to the client
 		pw.println("<h1>Hello Web World!!</h1>");
 	}
 
+	// Handles HTTP POST requests by delegating to doGet()
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {		
 		doGet(request, response);
